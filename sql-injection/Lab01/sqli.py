cfbcb2bdb9f6eb6e527069f3fdf4ca1a4c0e9723
edaif <93865594+edaif@users.noreply.github.com>
2022-11-10 23:47:24 +0100

    Lab01 script

--- sql-injection/Lab01/sqli.py
+++ sql-injection/Lab01/sqli.py
@@ -4,6 +4,7 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 proxies ={'http' : 'http:127.0.0.1:8080' , 'https' : 'http://127.0.0.1:8080'}
+
 def exploit_sqli(url , payload):
     uri='/filter?category='
     r=requests.get(url + uri + payload , verify=False,proxies=proxies)
